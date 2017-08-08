074a4fba3f989fe462ef08ba48dcd1bbc400627a
Bachchu3108 <30470036+Bachchu3108@users.noreply.github.com>
2017-08-08 08:26:44 +0700

    Update game.py

--- Homework5/game.py
+++ Homework5/game.py
@@ -8,19 +8,22 @@ gy = 3
 x = 0
 y = 0
 
+#map
 def _map(x, y, px, py, gx, gy, bx, by):
      for y in range(4):
           for x in range(4):
                if x == px and y == py:
-                    print("P", end = " ")
+                    print(" P ", end = " ")
                elif x == gx and y == gy:
-                    print("G", end = " ")
+                    print(" G ", end = " ")
                elif x == bx and y == by:
-                    print("B", end = " ")
+                    print(" B ", end = " ")
                else:
-                    print("-", end = " ")
+                    print(" - ", end = " ")
           print()
 _map(x, y, px, py, gx, gy, bx, by)
+
+
 #command
 while bx != gx or by != gy:
      command = str(input("Move??"))
@@ -28,32 +31,44 @@ while bx != gx or by != gy:
           if px == 3:
                print("cannot go forward!!")
           elif py == by and px + 1 == bx:
-               px += 1
-               bx += 1
+               if bx == 3:
+                    print("cannot go forward!!")
+               else:
+                    px += 1
+                    bx += 1
           else:
                px += 1
      elif command.upper() == "S":
           if py == 3:
                print("cannot go down!!")
           elif px == bx and py + 1 == by:
-               py += 1
-               by += 1
+               if py == 3:
+                    print("cannot go down!!")
+               else:
+                    py += 1
+                    by += 1
           else:
                py += 1
      elif command.upper() == "A":
           if px == 0:
                print("cannot go backward!!")
           elif py == by and px - 1 == bx:
-               px -= 1
-               bx -= 1
+               if bx == 0:
+                    print("cannot go backward!!")
+               else:
+                    px -= 1
+                    bx -= 1
           else:
                px -= 1
      elif command.upper() == "W":
           if py == 0:
                print("cannot go up!!")
           elif px == bx and py - 1 == by:
-               py -= 1
-               by -= 1
+               if by == 0:
+                    print("cannot go up!!")
+               else:
+                    py -= 1
+                    by -= 1
           else:
                py -= 1
      #hack
