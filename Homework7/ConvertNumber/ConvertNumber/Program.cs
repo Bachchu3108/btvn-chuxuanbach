494edc1a7d30192f4b1a3e553b214fa66abb8525
DESKTOP-VI76US8\VTS-Laptop <bachchu3108@gmail.com>
2017-08-13 22:55:23 +0700

    Session 7 homework commit

---
+++ Homework7/ConvertNumber/ConvertNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConvertNumber
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			List<int> numbers = new List<int> ();
+			Random rnd = new Random();
+			int n = 10;
+
+			for (int i = 0; i < n; i++) {
+				numbers.Add (rnd.Next (1, 100));
+				
+			}
+			for (int i = 0; i < n; i++) {
+				int decimalNumber = numbers [i];
+				int remainder;
+				string result = string.Empty;
+				while (decimalNumber > 0)
+				{
+					remainder = decimalNumber % 2;
+					decimalNumber /= 2;
+					result = remainder.ToString() + result;
+				}
+				Console.WriteLine("Decimal : {0} and Binary:  {1}",numbers[i],result);
+			}
+		}
+	}
+}
---
+++ Homework7/GCD/GCD/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace GCD
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			List<int> numbers = new List<int> ();
+			Random rnd = new Random();
+			int n = 10;
+
+			for (int i = 0; i < n; i++) {
+				numbers.Add (rnd.Next (1, 100));
+				Console.WriteLine (numbers [i]);
+
+			}
+
+			int k = numbers [0];
+			for (int i = 1; i < n; i++) {
+				if (k > numbers[i]){
+					k = numbers[i];
+			}
+
+		}
+			Console.WriteLine(k);
+	}
+}
+}
