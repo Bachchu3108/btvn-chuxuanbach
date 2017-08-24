03d9670b70b63dd518c0d647afc90d752b568762
DESKTOP-VI76US8\VTS-Laptop <bachchu3108@gmail.com>
2017-08-24 22:29:14 +0700

    Session 9 homework commit

---
+++ Homework9/God Tower Chu Xuân Bách/Assets/Scripts/ScenceScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScenceScript : MonoBehaviour {
+
+	public Text levelText;
+	public InputField inputField;
+	public Button summitButton;
+	public Text HintText;
+
+	public string levelContent = "LEVELS";
+	public string levelnumber;
+	public string levelAnswer;
+
+	string answer;
+
+	// Use this for initialization
+	void Start () {
+		levelText.text = levelContent;
+		StartCoroutine (ChangeLevelTextRoutine ());
+	}
+
+
+
+	public void getInput(){
+		answer = inputField.text;
+		CheckAnswer (answer);
+	}
+
+	public void CheckAnswer(string answer){
+		if (answer.ToLower() == levelAnswer) {
+			HintText.text = "Yayyyyyyyy !!";
+
+			//TODO: Change Scene
+
+		}else{
+			HintText.text = "WRONG !!!!";
+			HintText.color = Color.red;
+			inputField.text = "";
+			inputField.ActivateInputField ();
+		}
+	}
+
+
+	IEnumerator ChangeLevelTextRoutine(){
+		while (true) {
+			levelText.text = levelContent;
+			yield return new WaitForSeconds (2f);
+			levelText.text = levelnumber;
+			yield return new WaitForSeconds (2f);
+		}
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
